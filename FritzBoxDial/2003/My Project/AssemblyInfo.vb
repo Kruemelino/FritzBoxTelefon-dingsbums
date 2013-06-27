b16d32640ce43356d4b7bd12dfe41e0af5a951ae
Kruemelino <kruemelino@online.de>
2013-06-27 11:47:47 +0200

    Office 2003 pfx verschoben

--- FritzBoxDial/2003/My Project/AssemblyInfo.vb
+++ FritzBoxDial/2003/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-<Assembly: Guid("fa51a94c-3b86-4411-a4aa-d1eafb93a9c2")> 
+<Assembly: Guid("411894A1-05D5-4F89-B336-4A4175D5E537")> 
 
 ' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.4.3.0")> 
-<Assembly: AssemblyFileVersion("3.4.3.0")> 
+<Assembly: AssemblyVersion("3.4.3.6")> 
+<Assembly: AssemblyFileVersion("3.4.3.6")> 
