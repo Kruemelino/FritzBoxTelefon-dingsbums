568689c41a41b69cae938fdb891b39bcb18438c0
Kruemelino <kruemelino@online.de>
2013-09-07 12:02:51 +0200

    Office 2013 Update

--- FritzBoxDial/2013/My Project/AssemblyInfo.vb
+++ FritzBoxDial/2013/My Project/AssemblyInfo.vb
@@ -37,6 +37,6 @@ Imports System.Security
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.4.4.0")> 
-<Assembly: AssemblyFileVersion("3.4.4.0")> 
+<Assembly: AssemblyVersion("3.4.4.4")> 
+<Assembly: AssemblyFileVersion("3.4.4.4")> 
 <Assembly: NeutralResourcesLanguageAttribute("de-DE")> 
