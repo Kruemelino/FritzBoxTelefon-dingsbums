ff45085502992a95eb04160b4f03ecee15a8bec3
Kruemelino <kruemelino@online.de>
2014-05-15 22:35:40 +0200

    Verschlüsselung #3

--- FritzBoxDialDebug/FormMain.vb
+++ FritzBoxDialDebug/FormMain.vb
@@ -66,7 +66,7 @@ Public Class FormMain
         C_DP.P_TBBenutzer = Me.TBBenutzer.Text
         C_DP.P_TBVorwahl = Me.TBVorwahl.Text
         If Not Me.TBPasswort.Text = "1234" Then
-            C_DP.P_TBPasswort = C_Crypt.EncryptString(Me.TBPasswort.Text, C_DP.P_Def_PassWordDecryptionKey)
+            C_DP.P_TBPasswort = C_Crypt.EncryptString128Bit(Me.TBPasswort.Text, C_DP.P_Def_PassWordDecryptionKey)
             C_DP.SaveSettingsVBA("Zugang", C_DP.P_Def_PassWordDecryptionKey)
             C_Helfer.KeyChange()
         End If
