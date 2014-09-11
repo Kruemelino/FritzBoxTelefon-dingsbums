13a2f50b078f65c558299b56baf68a0efdf92a51
Kruemelino <kontakt@gert-michael.de>
2014-09-11 19:18:59 +0200

    Office 2003 Versionsnummer

--- Funktionen/My Project/AssemblyInfo.vb
+++ Funktionen/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.7.6.0")> 
-<Assembly: AssemblyFileVersion("3.7.6.0")> 
+<Assembly: AssemblyVersion("3.7.7.0")> 
+<Assembly: AssemblyFileVersion("3.7.7.0")> 
