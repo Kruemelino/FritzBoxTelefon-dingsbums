88a8e0ea1b42f2ecf111e14bd63c188ab053a7c0
Kruemelino <kontakt@gert-michael.de>
2014-11-14 18:21:08 +0100

    Office 2013 Version 3.7.10

--- PopupFenster/My Project/AssemblyInfo.vb
+++ PopupFenster/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 
-<Assembly: AssemblyVersion("3.7.9.0")> 
-<Assembly: AssemblyFileVersion("3.7.9.0")> 
+<Assembly: AssemblyVersion("3.7.10.0")> 
+<Assembly: AssemblyFileVersion("3.7.10.0")> 
 
