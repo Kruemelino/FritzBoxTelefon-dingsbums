af2ea6b739225de6ea8e4f426e079443509a076d
Gert Michael <kontakt@gert-michael.de>
2018-03-16 08:05:32 +0100

    Versionsnummer

--- FritzBoxUPnP/My Project/AssemblyInfo.vb
+++ FritzBoxUPnP/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
-<Assembly: Guid("411894A1-05D5-4F89-B336-4A4175D5E537")> 
+<Assembly: Guid("411894A1-05D5-4F89-B336-4A4175D5E537")>
 
 ' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.9.13.0")> 
-<Assembly: AssemblyFileVersion("3.9.13.0")> 
+<Assembly: AssemblyVersion("3.9.19.0")>
+<Assembly: AssemblyFileVersion("3.9.19.0")>
