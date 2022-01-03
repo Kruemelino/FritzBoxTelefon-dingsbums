bcf137bc8e0b9d274b2ac3507a34a3df89549ce8
Gert Michael <kruemelino@gert-michael.de>
2022-01-03 09:23:36 +0100

    Fix: FBoxAPI LogExceptions werden nun in die Einstellungsdatei geschrieben

--- FBoxDial/Fritz!Box/FritzBoxStatus.vb
+++ FBoxDial/Fritz!Box/FritzBoxStatus.vb
@@ -2,6 +2,10 @@
 Friend Module FritzBoxStatus
     Private Property NLogger As Logger = LogManager.GetCurrentClassLogger
     Friend Sub FBoxAPIMessage(sender As Object, e As FBoxAPI.NotifyEventArgs(Of FBoxAPI.LogMessage))
-        NLogger.Log(LogLevel.FromOrdinal(e.Value.Level), e.Value.Message)
+        If e.Value.Ex Is Nothing Then
+            NLogger.Log(LogLevel.FromOrdinal(e.Value.Level), e.Value.Message)
+        Else
+            NLogger.Log(LogLevel.FromOrdinal(e.Value.Level), e.Value.Ex, e.Value.Message)
+        End If
     End Sub
 End Module
