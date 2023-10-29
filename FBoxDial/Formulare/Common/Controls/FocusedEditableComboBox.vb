a27a21bd113a4d9ba35fd41019cf7cc42e0ed3ab
Kruemelino <github@gert-michael.de>
2023-10-29 19:43:49 +0100

    Initialer Focus auf PART_EditableTextBox bei der Direktwahl

---
+++ FBoxDial/Formulare/Common/Controls/FocusedEditableComboBox.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Windows.Controls
+
+Public Class FocusedEditableComboBox
+    Inherits ComboBox
+
+    Public Overrides Sub OnApplyTemplate()
+        MyBase.OnApplyTemplate()
+        With CType(GetTemplateChild("PART_EditableTextBox"), TextBox)
+
+            ' Nicht schön, aber damit kann der Text Zentriert werden.
+            .TextAlignment = Windows.TextAlignment.Center
+
+            ' Setze den Fokus auf die TextBox
+            .Focus()
+        End With
+
+    End Sub
+
+End Class
